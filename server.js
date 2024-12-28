7dd2d9dd6c05f1bfb59124c1b34c0a04c5b391b8
ちと <chitose.dev@outlook.jp>
2024-12-28 20:10:22 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,65 @@
+import Mist from "@turbowarp/mist";
+import express from "express";
+import axios from "axios";
+import fs from "fs";
+
+let ws=new Mist({projectId:"1114861075",userAgent:"chito"});
+const char=JSON.parse(fs.readFileSync("char.json"));
+
+//リクエストが正常かどうかチェック
+const check=str=>{
+  if(!str[0]!="1")return false;
+  return true;
+};
+
+//リクエストからUNNを取得
+const getRequest=str=>{
+  const length=Number(str[1]+str[2])-10;
+  let name="";
+  let request="";
+  for(let i=0;i<length;i++)name+=str[i+3];
+  for(let i=0;i<str.length-length-3;i++)request+=str[i+length+3];
+  return {length,name,request};
+};
+const toStr=num=>{
+  let ans="";
+  for(let i=0;i<num.length;i+=3)ans+=char[Number(`${num[i]}${num[i+1]}${num[i+2]}`)-101];
+  return ans;
+};
+const toNum=str=>{
+  let num="";
+  for(let i=0;i<str.length;i++)num+=char.indexOf(str[i])+101;
+  return num;
+};
+
+const requesthandler=async num=>{
+  const dt=toStr(num);
+  const match=dt.match(/(?:https?:\/\/)?(?:www\.)?youtu(?:\.be\/|be\.com\/(?:watch\?v=|embed\/|v\/|shorts\/))([\w\-]+)/);
+  if(match){
+    const {data:response}=await axios.get(`https://wakamets.glitch.me/api/login/${match[1]}`);//一時的にわかめtubeのAPIを利用。後々自分でAPI立てるよてい
+    return toNum(response.stream_url);
+  };
+  return num;
+};
+
+ws.on("set",async(n,v)=>{
+  if(check(v))return;
+  const {length,name,request}=getRequest(v);
+  const response=`${length+10}${name}${await requesthandler(request)}`;
+  ws.set(n[2],`2${response}`)
+});
+
+const app=express();
+
+app.get("/",(req,res)=>{
+  res.send("Server is running");
+});
+
+app.get("/tostr/:num",(req,res)=>{
+  res.send(toStr(req.params.num));
+});
+
+const PORT = process.env.PORT || 7777;
+const listener=app.listen(PORT,()=>{
+  console.log(`Server is running on PORT ${listener.address().port}`);
+});
